587ffc237594ade4d68d958e309975357ddf8439
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-04-08 10:38:32 +0200

    ✅ Add best player votes api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/arffornia_v5/modlist', function () {
     return response()->file(public_path('files/ArfforniaV.5ModList.json'));
 });
+
+Route::get('best_player_vote/{size}', [UserController::class, 'bestPlayerByVote']);
