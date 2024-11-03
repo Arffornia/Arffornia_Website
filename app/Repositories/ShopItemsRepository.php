10dab1ff16bd10b6f071a78abc4ba90008aba8f8
brian.perret <brian.perret@epita.fr>
2024-11-03 18:57:37 +0100

    Add promotion by % of promotion

--- app/Repositories/ShopItemsRepository.php
+++ app/Repositories/ShopItemsRepository.php
@@ -1,29 +1,32 @@
 <?php
+
 namespace App\Repositories;
 
 use App\Models\ShopItem;
 
-class ShopItemsRepository {
+class ShopItemsRepository
+{
     public function getBestSellers(int $size)
     {
         return ShopItem::withCount('userSales')
-                        ->orderByDesc('user_sales_count')
-                        ->limit($size)
-                        ->get();
+            ->orderByDesc('user_sales_count')
+            ->limit($size)
+            ->get();
     }
 
     public function getNewest(int $size)
     {
         return ShopItem::latest()
-                        ->limit($size)
-                        ->get();
+            ->limit($size)
+            ->get();
     }
 
     public function getSales(int $size)
     {
-        return ShopItem::where('promo_price', '>=', '0')
-                        ->inRandomOrder()
-                        ->limit($size)
-                        ->get();
+        return ShopItem::where('promo_price', '>', 0)
+            ->whereColumn('promo_price', '<', 'real_price')
+            ->orderByRaw('(real_price - promo_price) / real_price DESC')
+            ->limit($size)
+            ->get();
     }
 }
