e3a8672d4426e53efb043a64b83d5321f231601e
brian.perret <brian.perret@epita.fr>
2025-04-03 13:30:09 +0200

    ✅ Migrate Laravel to pgsql

--- database/migrations/2024_04_11_103425_create_milestone_closure_table.php
+++ database/migrations/2024_04_11_103425_create_milestone_closure_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('milestone_closure', function (Blueprint $table) {
+            $table->id();
             $table->unsignedBigInteger("milestone_id");
             $table->unsignedBigInteger("descendant_id");
         });
