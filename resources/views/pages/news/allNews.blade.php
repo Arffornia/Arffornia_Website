2ab4a875ca642245044250d12fb46ac590290482
brian.perret <brian.perret@epita.fr>
2025-09-24 19:58:18 +0200

    feat: improve news + podium css

--- resources/views/pages/news/allNews.blade.php
+++ resources/views/pages/news/allNews.blade.php
@@ -2,21 +2,34 @@
 
 @section('extraHead')
     <link rel="stylesheet" href="{{ asset('css/pages/news/allNews.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components/newsSection.css') }}">
 @endsection
 
 @section('content')
     <div class="standart-page">
-        <div class="newsContainer">
-            @foreach ($newsList as $news)
-                <a class="news" href="news/{{ $news->id }}" title="Show news">
-                    <img class="newsImg" src="{{ asset($news->imgUrl) }} ">
-                    <div class="textContainer">
-                        <p class="title">{{ $news->title }}</p>
-                    </div>
-                </a>
-            @endforeach
+        <div class="news-section">
+            <div class="info">
+                <p class="title-midle">Toutes les News</p>
+            </div>
+
+            <div class="content">
+                @foreach ($newsList as $news)
+                    <a href="news/{{ $news->id }}" title="Show news" class="block-link">
+                        <div class="block">
+                            <div class="img-container">
+                                <img src="{{ $news->imgUrl }}" alt="{{ $news->title }}" class="block-image" />
+                            </div>
+                            <div class="block-text">
+                                <p class="title-mini">{{ $news->title }}</p>
+                                <p class="text-less description">{{ Str::limit(strip_tags($news->content), 50) }}</p>
+                            </div>
+                        </div>
+                    </a>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
+
 @section('script')
 @endsection
